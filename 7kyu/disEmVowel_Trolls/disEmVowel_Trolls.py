3215207f0416f4ade34cc0b810d1756bb4ddad97
borkusgod <lightraven.bourque@gmail.com>
2022-07-18 09:48:50 -0300

    Copied from previous project into new with Git setup from get go

--- 7kyu/disEmVowel_Trolls/disEmVowel_Trolls.py
+++ 7kyu/disEmVowel_Trolls/disEmVowel_Trolls.py
@@ -1,46 +1,23 @@
+# def disemvowel(string_):
+#     str_no_vow = ''
+#     vowel_list = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+#     find_words = string_.split(' ')
+#     # print(find_words)
+#     for each in find_words:
+#         for each_let in each:
+#             if not each_let in vowel_list:
+#                 str_no_vow += each_let
+#         str_no_vow += ' '
+#     print(type(str_no_vow))
+#     return str_no_vow
+
 def disemvowel(string_):
-    print(string_)
-    raw_s = f'\"{string_}\"'
-    print(raw_s)
-    vowel_list = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
-    split_into_words = string_.split(' ')
-    formatted_string = ''
-    for each in split_into_words:
-        word_formatted = ''
-        for letter in each:
-            if letter in vowel_list:
-                x = letter.replace(letter, '')
-                letter = x
-            if letter:
-                word_formatted += letter
-        formatted_string += word_formatted
-        formatted_string += ' '
-    strip_down = formatted_string.strip()
-    return strip_down
+    vowels = 'aeiouAEIOU'
+    # found this solution at: https://www.youtube.com/watch?v=SDbT8rBr8XI
+    return ''.join(filter(lambda char: char not in vowels, string_))
 
 
-string_to_test = 'looking for a string with consonants and vowels.'
-string_with_uppers = 'looking For a String that has Uppers and lowers'
-randoms = " -zE-JSUzAuEI_u OWIz^CaSoA_VaWeIOMuu?}In< EcXau)eu n@E a  ' \
-          'mx*oNU'): '-z-JSz_ Wz^CS_VWM?}n< cX) n@   mx*N"
-randoms2 = """
-EVzaoFlqcnQBC;~Ou qIez~'AEFw b "): "VzFlqcnQBC;~ qz~'Fw b
-"""
-# randoms4 = 'iqiOgE?tiOCK[AalOem i'): 'qg?tCK[lm'
-# randoms3 = """
-# \\oEOXoOWaeo&v,ELFae'): '\\XW&v,LF
-# """
-# randoms4 = '\\oEOXoOWaeo&v,ELFae'): '\\XW&v,LF'
-# remove_vowels(string_to_test)
+test_string1 = 'This website is for losers LOL!'
 
-print(string_to_test)
-print(disemvowel(string_to_test))
-print(string_with_uppers)
-print(disemvowel(string_with_uppers))
-# print(disemvowel(randoms))
-print(randoms2)
-print(disemvowel(randoms2))
-# print(randoms3)
-# # print(disemvowel(randoms3))
-# print(randoms4)
-# print(disemvowel(randoms4))
+print(test_string1)
+print(disemvowel(test_string1))
--- scratch.py
+++ scratch.py
@@ -1,19 +1,1 @@
-# n0v4s3c — Today at 7:01 PM
-# In c++ you would pass the argument by reference to modify to the original array.
-# In python, I think you use [:] so in your function do this
-# wealth[:] = modifiedList
-# Where modifiedList is your modified list.
-#
-# n0v4s3c — Today at 7:12 PM
-# /run python
-
-
-def redistribute(wealth):
-    wealth[:] = [sum(wealth)/len(wealth) for i in range(len(wealth))]
-
-
-list = [5, 6, 10]
-redistribute(list)
-print(list)
-
 
