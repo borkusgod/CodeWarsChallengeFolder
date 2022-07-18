131824f886addccb6b765c49633e2c7543b2a594
borkusgod <lightraven.bourque@gmail.com>
2022-07-18 11:29:59 -0300

    Copied from previous project into new with Git setup from get go

--- 7kyu/get_middle_char/get_middle_char.py
+++ 7kyu/get_middle_char/get_middle_char.py
@@ -1,6 +1,27 @@
 def get_middle(s):
-    #your code here
-    pass
+    print(s)
+    # get len first
+    if len(s) == 1 or len(s) == 2:
+        return s
+    len_of_s = len(s)
+    if (len_of_s % 2) == 0:
+        print('input string has even characters')
+        get_half = len_of_s / 2
+        # print(get_half)
+        left_ind = (int(get_half)) - 1
+        right_ind = int(get_half)
+        mid_l = s[left_ind]
+        mid_r = s[right_ind]
+        return f'{mid_l}{mid_r}'
+
+    if (len_of_s % 2) == 1:
+        print('input string has odd characters')
+        get_half = len_of_s / 2
+        # print(get_half)
+        gh_2_int = int(get_half)
+        # print(gh_2_int)
+        # print(s[gh_2_int])
+        return s[gh_2_int]
 
 
 string1 = 'test'
--- scratch.py
+++ scratch.py
@@ -1,1 +1,9 @@
+import math
+from math import ceil
 
+
+test_float = 2.5
+rnd_up = round(test_float)
+print(rnd_up)
+from_ceil = math.ceil(test_float)
+print(from_ceil)
