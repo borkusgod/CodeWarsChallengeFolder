e048f31f7b83d46804f7a63b0f180d4411a78b15
borkusgod <lightraven.bourque@gmail.com>
2022-07-21 14:31:05 -0300

    Copied from previous project into new with Git setup from get go

---
+++ Python/7kyu/credit_card_mask/credit_card_mask.py
@@ -0,0 +1,10 @@
+# return masked string
+def maskify(cc):
+    pass
+
+cc1 = 4450500016172487
+cc2 = 4450008092914556
+string_test1 = 'reeeeeaaaaaaalllllllllyyyyyyyyyyyyyy'
+string_test2 = 'skippyTheSpaceCowboy'
+
+maskify()
