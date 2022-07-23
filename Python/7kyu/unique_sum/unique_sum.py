5a75934ca81fa2ab6e5aeebc99585ebaec9c0729
borkusgod <lightraven.bourque@gmail.com>
2022-07-22 22:54:45 -0300

    Copied from previous project into new with Git setup from get go

--- Python/7kyu/unique_sum/unique_sum.py
+++ Python/7kyu/unique_sum/unique_sum.py
@@ -2,7 +2,11 @@ def unique_sum(lst):
     pass
 
 
-list1 = [ 1, 2, 3]
-list2 = [ 1, 3, 8, 1, 8]
-list3 = [ -1, -1, 5, 2, -7]
+list1 = [1, 2, 3]
+list2 = [1, 3, 8, 1, 8]
+list3 = [-1, -1, 5, 2, -7]
+
+print(unique_sum(list1))
+print(unique_sum(list2))
+print(unique_sum(list3))
 
